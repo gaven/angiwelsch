1bedfbcdeac44958c3370c04a76635825eae2c9a
Gaven Heim <gavenheim@gmail.com>
2017-01-15 21:27:57 -0700

    Begins building out grid plugin

--- _app/scripts/global.js
+++ _app/scripts/global.js
@@ -1,6 +1,8 @@
 import './plugins/jquery.toggle.js';
-
+import Grid from './plugins/jquery.grid.js';
 
 $(() => {
   $('.site-header__toggle').toggle();
+
+  new Grid;
 });
--- _app/scripts/plugins/jquery.grid.js
+++ _app/scripts/plugins/jquery.grid.js
@@ -1,5 +1,22 @@
+// import isotope from 'isotope-layout';
+import loaded from 'imagesloaded';
 
 class Grid {
   constructor () {
+    $(window).on('load', this.duplicateImage());
+  }
+
+  duplicateImage () {
+    const $wrapper = $('.grid__wrapper');
+    const $items = $wrapper.find('.grid__item');
+
+    $items.each(function () {
+      $(this).loaded(() => {
+        const $image = $(this).find('.grid__image');
+      });
+    });
   }
 }
+
+
+export default Grid;
