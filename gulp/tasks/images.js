b9c5b41619f5fc6005e9605b7dc3e18ec09e3e7f
Gaven Heim <gavenheim@gmail.com>
2017-01-25 18:40:37 -0700

    Adds gulp task to handle image minification

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,12 +1,25 @@
 import gulp from 'gulp';
+import resize from 'gulp-image-resize';
 import loadPlugins from 'gulp-load-plugins';
 
 const $ = loadPlugins();
 
 const images = () => {
-  return gulp.src('./_app/images/*')
-    .pipe($.imagemin())
-    .pipe(gulp.dest('./images'));
-}
+  return gulp.src('./images/uploads/*')
+    .pipe(resize({
+      height: 630,
+      crop: false,
+      upscale: false,
+      quality: 0.8
+    }))
+    .pipe(gulp.dest('./thumbs/small/images/uploads/'))
+    .pipe(resize({
+      height: 1304,
+      crop: false,
+      upscale: false,
+      quality: 0.8
+    }))
+    .pipe(gulp.dest('./thumbs/large/images/uploads/'))
+};
 
 gulp.task('build:images', images);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,6 @@ requireDir('./gulp/tasks', { recurse: true });
 const watch = () => {
   gulp.watch(['./_app/styles/**/*.scss'], ['build:styles']);
   gulp.watch(['./_app/scripts/**/*.js'], ['build:scripts']);
-  gulp.watch(['./_app/images/*'], ['build:images']);
   gulp.watch(['**/*.html', '**/*.md', '**/*.yml', '!_site/**/*.*'], ['build:reload']);
   gulp.watch(['./css/*.css', './scripts/*.js'], ['build:reload']);
 };
