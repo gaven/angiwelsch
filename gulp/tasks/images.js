9f6a37bdc6ce17b5ffd92234dd1f08eb4c537325
Gaven Heim <gavenheim@gmail.com>
2017-01-26 17:02:47 -0700

    Okay this works a lot better

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -4,18 +4,27 @@ import loadPlugins from 'gulp-load-plugins';
 
 const $ = loadPlugins();
 
-const images = () => {
+const smallImages = () => {
   return gulp.src('./images/uploads/*')
     .pipe($.changed('./thumbs/small/images/uploads/'))
     .pipe(resize({
       height: 630
     }))
-    .pipe(gulp.dest('./thumbs/small/images/uploads/'))
-    .pipe($.changed('./thumbs/large/images/uploads/'))
-    .pipe(resize({
-      height: 1404
-    }))
-    .pipe(gulp.dest('./thumbs/large/images/uploads/'))
+    .pipe(gulp.dest('./thumbs/small/images/uploads/'));
+};
+
+const largeImages = () => {
+  return gulp.src('./images/uploads/*')
+  .pipe($.changed('./thumbs/large/images/uploads/'))
+  .pipe(resize({
+    height: 1404
+  }))
+  .pipe(gulp.dest('./thumbs/large/images/uploads/'));
+};
+
+const images = () => {
+  smallImages();
+  largeImages();
 };
 
 gulp.task('build:images', images);
